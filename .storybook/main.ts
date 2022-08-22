c686f212cbe62c16f244ab5c3c942b87494acfb6
nathanjessen <nathan.jessen@gmail.com>
2022-08-21 21:10:26 -0500

    Addon for storybook test coverage

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,6 +5,7 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@storybook/preset-create-react-app',
+    '@storybook/addon-coverage',
   ],
   framework: '@storybook/react',
   core: {
