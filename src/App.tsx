5ddd876bca0e258d97ed40d913f8c17fcb54c413
Nathan Jessen <nathan.jessen@aplaceformom.com>
2024-11-29 01:23:01 -0600

    remove testing libraries and replace react-location with react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import { Outlet, ReactLocation, Router } from 'react-location';
-import { LocationGenerics, routes } from './router/routes';
-
-const reactLocation = new ReactLocation<LocationGenerics>();
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
+import { PATH_HOME, PATH_PRICING, PATH_RESULTS } from './constants/paths';
+import LandingPage from './pages/LandingPage';
+import Pricing from './pages/Pricing';
+import SearchResults from './pages/SearchResults';
 
 function App() {
   return (
-    <div className='App'>
-      <Router location={reactLocation} routes={routes}>
-        <Outlet />
+    <div className='App' role="main" aria-label="app">
+      <Router basename="/">
+        <Routes>
+          <Route path={PATH_HOME} element={<LandingPage />} />
+          <Route path={PATH_PRICING} element={<Pricing />} />
+          <Route path={PATH_RESULTS} element={<SearchResults />} />
+        </Routes>
       </Router>
     </div>
   );
